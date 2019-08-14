af95a84ae73f382ec2dab5b021fd9af606054654
Isaac McDaniel <mcdanielisaac@users.noreply.github.com>
2019-08-14 13:07:10 -0400

    Update config-gatsby.js and .gitignore file

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,22 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: process.env.DEPLOY_URL
+          ? "https://nameless-gorge-16856.herokuapp.com"
+          : "http://localhost:1337",
+        contentTypes: [
+          'activity',
+          'location',
+          'sponsor',
+          'tag',
+          'user'
+        ],
+        queryLimit: 1000,
+      },
+    },
+    {
       resolve: 'gatsby-source-strapi',
       options: {
         apiURL: 'http://localhost:1337',
